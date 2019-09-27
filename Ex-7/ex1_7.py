6a4e3b98829bb6e3f0f749c6cd7cf95ada4ef409
sudipto baral <sudiptobaral.me@gmail.com>
2019-09-27 16:17:15 +0600

    exercise 7 is completed

---
+++ Ex-7/ex1_7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb")
+print("Its fleece was white as {}. ".format('snow'))
+print("And everywhere that Mary went")
+#prints 10 "."
+print("."*10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# By default print() function ads a newline after the output
+# end = ' ' is manually specifying what to add at the end of the output
+# here it adds a whitespace
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
