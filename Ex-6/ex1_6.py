dbc9900282b3d7cedd86736543919ff382c66ba1
sudipto baral <sudiptobaral.me@gmail.com>
2019-09-27 15:54:19 +0600

    Exercise 6 completed

---
+++ Ex-6/ex1_6.py
@@ -0,0 +1,35 @@
+# type of people we have
+types_of_people = 10
+# x is a string which contains the text: There are 10 types of people
+x = f"There are {types_of_people} types of people."
+# contains string
+binary = "binary"
+# Another string variable
+do_not = "don't"
+# Building a string using formatting
+y = f"Those who know {binary} and those who {do_not}."
+#prints value of x
+print(x)
+#prints the value of y
+print(y)
+# Prints x along with some other value using formatting
+print(f"I said: {x}")
+# Prints y along with some other value using formatting
+print(f"I also said: '{y}'")
+# Boolean variable initialized as False
+hilarious = False
+# Another string
+joke_evaluation = "Isn't that joke so funny?! {}"
+#printing using string.format() method
+print(joke_evaluation.format(hilarious))
+#Another two stings
+w = "This is the left side of..."
+e = "a string with a right side."
+# printing after concatenating them usng + operator
+print(w + e)
+
+############ STUDY DRILL ##############
+# 1. Done
+# 2. Places : line 4, 10, 16, 18, 22
+# 3. 5 Places
+# 4. Because + operator on string works as concatanation operation
