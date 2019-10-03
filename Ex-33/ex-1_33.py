e5584a1615613e48cd4b0b007ab626027f7444ee
sudipto baral <sudiptobaral.me@gmail.com>
2019-10-04 00:26:16 +0600

    Done upto 35! lots of typing

---
+++ Ex-33/ex-1_33.py
@@ -0,0 +1,13 @@
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top of i is {i}")
+    numbers.append(i)
+    i+=1
+    print("Numbers now : ",numbers)
+    print(f"At the bottom i is {i}")
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
