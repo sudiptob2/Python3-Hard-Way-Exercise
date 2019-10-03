4862182421125103220bf0b26a3b5d94ca682fbc
sudipto baral <sudiptobaral.me@gmail.com>
2019-10-03 14:43:24 +0600

    done upto exercise 31, Lots of typing python

---
+++ Ex-29/ex1_29.py
@@ -0,0 +1,21 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+if people > cats:
+    print("Not many cats! The world is saves!")
+if people < dogs:
+    print("The world is drooled on!")
+if people > dogs:
+    print("Tge world is dry")
+
+dog += 5
+
+if people >= dogs:
+    print("People are grater than or equal to dogs.")
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+if people == dogs:
+    print("People are dogs.")
